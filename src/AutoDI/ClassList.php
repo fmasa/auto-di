c377483a5150b1a2c11b2975de85447270cf28bf
František Maša <frantisekmasa1@gmail.com>
2017-12-24 02:39:23 +0100

    Use strict_types

--- src/AutoDI/ClassList.php
+++ src/AutoDI/ClassList.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Fmasa\AutoDI;
 
 class ClassList
--- tests/DI/AutoDIExtensionTest.php
+++ tests/DI/AutoDIExtensionTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Fmasa\AutoDI\DI;
 
 use Nette\Configurator;
